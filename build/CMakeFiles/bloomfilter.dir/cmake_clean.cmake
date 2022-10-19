3a50c04387883e8de5ec67ebc819c422cb71690f
Zachary Ross <zachross015@gmail.com>
2022-10-18 20:35:41 -0400

    Added build files

---
+++ build/CMakeFiles/bloomfilter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bloomfilter.dir/bloomfilter.cpp.o"
+  "bloomfilter"
+  "bloomfilter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bloomfilter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
